ddcc498664b87b19ccf081d6eef769e358e8315d
merulis <merulis@yandex.ru>
2024-10-31 14:06:20 +0300

    create settings file

---
+++ system/settings.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file="../.env",
+        env_ignore_empty=True,
+        extra=True,
+    )
+
+    MQTT_BROKER_HOST: str
+    MQTT_BROKER_PORT: int
+    MQTT_BROKER_USER: str
+    MQTT_BROKER_PASSWORD: str
