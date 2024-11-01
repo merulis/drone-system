de1077a0a5d97d52422b9bc194dc4763b8b99c68
merulis <merulis@yandex.ru>
2024-11-01 17:08:23 +0300

    client init from my MQTTClient.start_connetion()

--- system/client/main.py
+++ system/client/main.py
@@ -1,48 +1,14 @@
-import logging
-
+from system.client.brocker import MQTTClientDroneSide
 from system.core.settings import settings
 
-import paho.mqtt.client as mqtt
-from paho.mqtt.client import MQTTMessage
 
-
-def on_connect(client, userdata, falgs, rc, props):
-    logger.info(f"Connected with result code {str(rc)}")
-
-
-def on_subscribe(client, userdata, mid, rc, props):
-    logger.info(f"Subscribe - result code {str(rc)}")
-
-
-def on_message(client, userdata, message: MQTTMessage):
-    logger.info(f"Message: {message.payload.decode()}")
-
-
-def on_disconnet(client, userdata, falgs, rc, props):
-    logger.info(f"Disconnect with result code {str(rc)}")
-
-
-logging.basicConfig(level=logging.INFO)
-
-subscriber = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-
-subscriber.on_connect = on_connect
-subscriber.on_subscribe = on_subscribe
-subscriber.on_message = on_message
-subscriber.on_disconnect = on_disconnet
-
-logger = logging.getLogger(__name__)
-subscriber.enable_logger(logger)
-
-subscriber.username_pw_set(
+subscriber = MQTTClientDroneSide()
+subscriber.start_connetion(
     username=settings.MQTT_BROKER_USER,
     password=settings.MQTT_BROKER_PASSWORD,
-)
-subscriber.connect(
     host=settings.MQTT_BROKER_HOST,
     port=settings.MQTT_BROKER_PORT,
 )
 
 subscriber.subscribe(topic="test", qos=0)
-
 subscriber.loop_forever()
--- system/server/main.py
+++ system/server/main.py
@@ -1,40 +1,13 @@
 import time
-import logging
 
 from system.core.settings import settings
-
-import paho.mqtt.client as mqtt
-
-
-def on_connect(client, userdata, falgs, rc, props):
-    logger.info(f"Connected with result code {str(rc)}")
+from system.server.brocker import MQTTClientBackendSide
 
 
-def on_publish(client, userdata, mid, rc, props):
-    logger.info(f"Publish - message ID: {mid}, result code {str(rc)}")
-
-
-def on_disconnet(client, userdata, falgs, rc, props):
-    logger.info(f"Disconnect with result code {str(rc)}")
-
-
-logging.basicConfig(level=logging.INFO)
-
-publisher = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-
-
-publisher.on_connect = on_connect
-publisher.on_publish = on_publish
-publisher.on_disconnect = on_disconnet
-
-logger = logging.getLogger(__name__)
-publisher.enable_logger(logger)
-
-publisher.username_pw_set(
+publisher = MQTTClientBackendSide()
+publisher.start_connetion(
     username=settings.MQTT_BROKER_USER,
     password=settings.MQTT_BROKER_PASSWORD,
-)
-publisher.connect(
     host=settings.MQTT_BROKER_HOST,
     port=settings.MQTT_BROKER_PORT,
 )
