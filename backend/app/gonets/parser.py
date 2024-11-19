631a3f22a32f641c34f843f56ac717805dc9bae5
merulis <merulis@yandex.ru>
2024-11-19 16:31:11 +0300

    add import cookies from selenium to aiohttp and request message list

--- backend/app/gonets/parser.py
+++ backend/app/gonets/parser.py
@@ -1,27 +1,39 @@
+import asyncio
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 from twocaptcha import TwoCaptcha
 
-import time
-import uuid
-import base64
+from aiohttp import ClientSession
 
 from app.core.settings import settings
 
 
-cookies_needs = [
-    "_ym_d",
-    "_ym_isad",
-    "_ym_uid",
-    "_ym_visorc",
-    "ASP.NET_SessionId",
-    "fullNameGS",
-    "trueUserV",
-    "userClientGS",
-    "userLoginGS",
-    "userNameGS",
-]
+def create_webdriver(
+    options: list | None = None,
+) -> webdriver.Chrome:
+    if not options:
+        options = ["--headless"]
+
+    driver_options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(options=driver_options)
+
+    [driver_options.add_argument(option) for option in options]
+
+    return driver
+
+
+def get_captcha_as_base64_or_none(driver: webdriver.Chrome):
+    try:
+        captcha = driver.find_element(By.ID, "CaptchaI")
+
+    except Exception:
+        return None
+
+    captcha_base64 = captcha.screenshot_as_base64
+
+    return captcha_base64
 
 
 def solve_captcha(encoded_data):
@@ -35,32 +47,7 @@ def solve_captcha(encoded_data):
         return result
 
 
-def get_captcha_path_or_none(driver: webdriver.Chrome):
-    try:
-        captcha = driver.find_element(By.ID, "CaptchaI")
-
-    except Exception:
-        return None
-
-    image_name = f"{str(uuid.uuid4())}.png"
-    image_path = settings.CAPTCHA.IMG_FOLDER / image_name
-    _ = captcha.screenshot(str(image_path))
-
-    return image_path
-
-
-def encode_captcha_img_bs64(image_path):
-    if not image_path.exists():
-        time.sleep(0.3)
-
-    with open(image_path, "rb") as file:
-        data = file.read()
-
-    encoded_data = base64.b64encode(data).decode("utf-8")
-    return encoded_data
-
-
-def fill_form_and_enter(driver, result):
+def fill_form_and_enter(driver: webdriver.Chrome, result):
     login_input = driver.find_element(By.ID, "TextBox_Login")
     password_input = driver.find_element(By.ID, "pass")
     capthca_input = driver.find_element(By.ID, "Captcha")
@@ -73,23 +60,63 @@ def fill_form_and_enter(driver, result):
     enter_button.click()
 
 
-def main():
-    driver = webdriver.Chrome()
-    driver.get(settings.GONETS.BASE_URL + settings.GONETS.LOGIN_URL)
+def get_cookies_for_aiohttp(
+    driver: webdriver.Chrome,
+) -> dict:
+    def set_cookie(cookies, cookie):
+        cookies.setdefault(cookie.get("name"), cookie.get("value"))
 
-    image_path = get_captcha_path_or_none(driver)
+    cookies = {}
+    [set_cookie(cookies, cookie) for cookie in driver.get_cookies()]
 
-    if not image_path:
+    return cookies
+
+
+def get_user_id_from_cookies(cookies):
+    return cookies.get(settings.GONETS.COOKIE_USER_LOGIN)
+
+
+async def get_messages(selenuim_cookies, user_id):
+    async with ClientSession(cookies=selenuim_cookies) as session:
+        url = settings.GONETS.BASE_URL + settings.GONETS.LIST_MESSAGE_ROUTE
+
+        json = settings.GONETS.LIST_MESSAGE_JSON
+        json[settings.GONETS.LIST_MESSAGE_USER_ID] = user_id
+
+        async with session.post(
+            url=url,
+            headers={
+                "Accept": "application/json, text/javascript, */*; q=0.01",
+                "Accept-Language": "en-US,en;q=0.5",
+                "Accept-Encoding": "gzip, deflate, br, zstd",
+                "Content-Type": "application/json; charset=utf-8,",
+            },
+            json=json,
+        ) as response:
+            status = response.status
+            message_json = await response.json()
+
+        return status, message_json
+
+
+async def main():
+    driver = create_webdriver()
+
+    driver.get(settings.GONETS.BASE_URL + settings.GONETS.LOGIN_ROUTE)
+
+    if not (encoded_captcha := get_captcha_as_base64_or_none(driver)):
         print("Captcha not found, go to main page")
 
-    encoded_captcha = encode_captcha_img_bs64(image_path)
     result = solve_captcha(encoded_captcha)
     fill_form_and_enter(driver, result)
 
-    cookies = [driver.get_cookie(cookie_name) for cookie_name in cookies_needs]
+    selenuim_cookies = get_cookies_for_aiohttp(driver)
+    user_id = get_user_id_from_cookies(selenuim_cookies)
+
     driver.quit()
 
-    print(cookies)
+    status, json = await get_messages(selenuim_cookies, user_id)
+    print(status, json)
 
 
-main()
+asyncio.run(main())
