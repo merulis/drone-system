e1508aaa3fa9ec93187ccfe23746a602e2e60540
merulis <merulis@yandex.ru>
2024-11-26 18:55:32 +0300

    add celery beat conf

--- backend/app/background/worker.py
+++ backend/app/background/worker.py
@@ -1,10 +1,11 @@
 from celery import Celery
+from celery.schedules import crontab
 
 from app.core.settings import settings
 from app.gonets.parser import parse_message
 
 
-celery = Celery("task", broker=settings.CELERY.BROCKER_URL)
+celery = Celery("task", broker=str(settings.CELERY.BROCKER_URL))
 
 
 @celery.task
@@ -12,3 +13,11 @@ async def get_message_from_gonets():
     result = await parse_message()
 
     return result
+
+
+celery.conf.beat_schedule = {
+    "every-5-minutes": {
+        "task": "tasks.add",
+        "schedule": crontab(minute="*/5"),
+    }
+}
