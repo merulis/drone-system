3ce0e4e5cfda56832f1aa37bd3c8cbed6973c12d
merulis <merulis@yandex.ru>
2024-12-10 19:14:46 +0300

    add celery beat task

--- backend/app/background/worker.py
+++ backend/app/background/worker.py
@@ -2,22 +2,24 @@ from celery import Celery
 from celery.schedules import crontab
 
 from app.core.settings import settings
-from app.gonets.parser import parse_message
+from app.gonets.main import get_gonets_info
 
 
 celery = Celery("task", broker=str(settings.CELERY.BROCKER_URL))
+remote_driver_url = str(settings.GONETS.REMOTE_DRIVER_URL)
+
+
+@celery.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(
+        crontab(minute="*/5"),
+        get_message_from_gonets.s(remote_driver_url),
+        name="get every 5 minutes",
+    )
 
 
 @celery.task
-async def get_message_from_gonets():
-    result = await parse_message()
+def get_message_from_gonets(url):
+    result = get_gonets_info(url)
 
     return result
-
-
-celery.conf.beat_schedule = {
-    "every-5-minutes": {
-        "task": "tasks.add",
-        "schedule": crontab(minute="*/5"),
-    }
-}
