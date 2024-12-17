4b962381a0e45dab9dc475ebf3900f42312614a5
merulis <merulis@yandex.ru>
2024-12-17 18:02:05 +0300

    remove old dependeces

--- backend/app/core/db/__init__.py
+++ backend/app/core/db/__init__.py
@@ -1,11 +1,8 @@
 from .base import Base
-from .message.model import Message
-from .database import DataBase, database
+from .database import database
 
 
 __all__ = (
     "Base",
-    "Message",
-    "DataBase",
     "database",
 )
