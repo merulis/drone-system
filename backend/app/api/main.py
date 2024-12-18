847a9c2a41d868a5267135d17469c54a48f84a0e
merulis <merulis@yandex.ru>
2024-12-18 15:25:27 +0300

    add trap exception and fix bug

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter
 
+from .routes.message import router as message
 
 router = APIRouter()
+
+router.include_router(router=message, tags=["Message"])
--- backend/app/api/routes/message.py
+++ backend/app/api/routes/message.py
@@ -1,6 +1,11 @@
+import logging
+import traceback
+
 from fastapi import (
     APIRouter,
     Depends,
+    HTTPException,
+    status,
 )
 
 from app.message.dependence import get_message_service
@@ -8,12 +13,31 @@ from app.message.services import IMessageService
 from app.message.schemas import Message
 
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - [%(levelname)s] - %(message)s"
+)
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
 router = APIRouter(prefix="/message")
 
 
-@router.get("/message")
+@router.get("/")
 async def get_messages(
     service: IMessageService = Depends(get_message_service),
 ) -> list[Message]:
-    messages = await service.get_messages()
-    return messages
+    try:
+        messages = await service.get_messages()
+    except Exception as e:
+        logger.error(f"failed to retrieve message: {e}")
+        logger.debug(traceback.format_exc())
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e),
+        )
+    else:
+        return messages
