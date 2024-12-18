97ef301cf0044e424abfc213b193a64676da2a4d
merulis <merulis@yandex.ru>
2024-12-18 17:51:40 +0300

    refactored [background] replace tasks in modules

--- backend/app/background/main.py
+++ backend/app/background/main.py
@@ -3,6 +3,9 @@ from celery import Celery
 from app.core.settings import settings
 
 
-celery = Celery("background", broker=str(settings.CELERY.BROCKER_URL))
+celery = Celery(
+    main="background",
+    broker=str(settings.CELERY.BROCKER_URL),
+)
 
-celery.autodiscover_tasks(["tasks"])
+celery.autodiscover_tasks()
